fbed9199a14e35408e03294e56edc89b7c86978e
Pablo Ruan <pablo.viana@sou.inteli.edu.br>
2024-02-29 11:53:15 -0300

    feat(themes): :sparkles: create script to move themes

---
+++ Themes/move-themes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+unzip Themes/themes.zip -d ~/.themes
+unzip Themes/pointer.zip -d ~/.icons
+unzip Themes/icons.zip -d ~/.local/share/icons
+unzip Themes/backgrounds.zip -d ~/.local/share/backgrounds
